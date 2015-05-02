80ea9a736c736bb77ee0313f21bb6a51e8ce6dda
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-05-02 11:22:28 +0200

    chore: fix .travis.yml; added test_results_database config

--- lib/travis/config/defaults.rb
+++ lib/travis/config/defaults.rb
@@ -15,6 +15,7 @@ module Travis
             assets:        { host: HOSTS[Travis.env.to_sym] },
             amqp:          { username: 'guest', password: 'guest', host: 'localhost', prefetch: 1 },
             database:      { adapter: 'postgresql', database: "travis_#{Travis.env}", encoding: 'unicode', min_messages: 'warning' },
+            test_results_database:      { adapter: 'postgresql', database: "travis_#{Travis.env}", encoding: 'unicode', min_messages: 'warning' },
             s3:            { access_key_id: '', secret_access_key: '' },
             pusher:        { app_id: 'app-id', key: 'key', secret: 'secret' },
             sidekiq:       { namespace: 'sidekiq', pool_size: 1 },
