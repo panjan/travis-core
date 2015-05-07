45c36511beddeb629165b55b1186f9b93105630e
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-05-07 12:52:12 +0200

    feature: implement fetch_content for StashProvider

--- lib/travis/model/repository_provider/stash_provider.rb
+++ lib/travis/model/repository_provider/stash_provider.rb
@@ -7,25 +7,49 @@ class StashProvider < RepositoryProvider
   end
 
   def source_url
-    "git@#{source_host}:#{stash_slug}.git"
+    "#{git_source_protocol}://git@#{source_host}:#{git_source_port}/" +
+      "#{repository.owner_name}/repos/#{repository.name}.git"
   end
 
   def content_url(options)
-    "https://#{source_host}/#{stash_slug}/browse/#{options[:path]}?at=#{options[:ref]}".to_s
+    #"#{source_protocol}://#{source_host}:#{source_port}/#{stash_slug}/browse/#{options[:path]}?at=#{options[:ref]}&raw".to_s
+    "#{source_protocol}://#{source_host}:#{source_port}/" +
+      "projects/#{repository.owner_name}/repos/#{repository.name}/" +
+      "browse/#{options[:path]}?at=#{options[:ref]}&raw".to_s
   end
 
-  def fetch_content(file_url)
+  def fetch_content(content_url)
+    @fetch_content ||= Faraday.new(content_url,  config.connection || {}).get.body
   end
 
   def source_host
-    (Travis.config.stash and Travis.config.stash.source_host) || raise('Travis.config.stash.source_host not provided')
+    config.source_host || raise('Travis.config.stash.source_host not provided')
+  end
+
+  def source_port
+    config.source_port || 443
+  end
+
+  def source_protocol
+    config.source_protocol || 'https'
+  end
+
+  def git_source_port
+    config.git_source_port || 22
+  end
+
+  def git_source_protocol
+    config.git_source_protocol || 'ssh'
   end
 
   private
 
+    def config
+      Travis.config.stash || {}
+    end
+
     def stash_slug
       #TODO: stash has projects and users
-      @stash_slug ||= ['projects', repository.owner_name, 'repos', repository.name].join('/')
     end
 
 end
